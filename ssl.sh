6f3dc4ac778b841b0e1df69cf52b6ab73a1384ba
psycosocial <p5yc050c14l@gmail.com>
2018-06-23 15:42:29 +0700

    Add files via upload

---
+++ ssl.sh
@@ -0,0 +1,8 @@
+apt-get install stunnel4 -y                                               
+wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/psycosocial/debian7/master/stunnel.conf"         
+openssl genrsa -out key.pem 2048                                          
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095              
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem                          
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4                    
+clear                                                                     
+/etc/init.d/stunnel4 restart
