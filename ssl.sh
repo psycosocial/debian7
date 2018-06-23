b9a9a915ac0613b1ce0a4de90ff7b7e1d7385dd8
psycosocial <p5yc050c14l@gmail.com>
2018-06-23 10:54:23 +0000

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,7 +1,10 @@
 apt-get -y install stunnel4
 wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/psycosocial/debian7/master/stunnel.conf"
+cd /etc/stunnel
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-/etc/init.d/stunnel4 restart
+cd
+rm ssl.sh
+services stunnel4 restart
